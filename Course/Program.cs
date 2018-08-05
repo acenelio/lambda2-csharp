b5b2027715134bca1f2077afa0f09dbc947a34a2
Nelio Alves <acenelio@gmail.com>
2018-08-05 09:52:25 -0300

    Solution with lambda expression

--- Course/Program.cs
+++ Course/Program.cs
@@ -13,14 +13,10 @@ namespace Course {
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            list.RemoveAll(ProductTest);
+            list.RemoveAll(p => p.Price >= 100.0);
             foreach (Product p in list) {
                 Console.WriteLine(p);
             }
-        }
-
-        public static bool ProductTest(Product p) {
-            return p.Price >= 100.0;
         }
     }
 }
