137fb7d50ad1f8f0b4b05d389c3edba512ba7bb7
Nelio Alves <acenelio@gmail.com>
2018-08-05 09:51:49 -0300

    Solution with reference method

---
+++ Course/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Course.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Price.ToString("F2");
+        }
+    }
+}
